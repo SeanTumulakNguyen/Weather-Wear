8d09f12d50f18826b1fe86fc860c16bbbd0f4bee
Juwami <sean.nguyenf@yahoo.com>
2019-06-04 21:30:15 -0400

    add function for precipitation and what type

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -5,6 +5,7 @@ let locationInput = document.getElementById('#').value
 
 let temp
 let precipChance
+let precipType = // this is from openweatherapi on type of precip. rain or snow?
 
 let clothesfromTemp = function() {
     if (temp >= 70) {
@@ -15,5 +16,23 @@ let clothesfromTemp = function() {
     }
     else (temp < 50) {
         // tryin' to eat some frostbite boi?
+    }
+}
+
+let accessoriesrfromPrecip = function () {
+    if (precipChance >= 60 && precipType === 'snow') {
+        // get your milk, eggs, and bread
+    }
+    else if (precipChance >= 60 && precipType === 'rain') {
+        // keep umbrella on you
+    }
+    else if (precipChance < 60 && precipChance >= 30 && precipType === 'snow') {
+        // shovels and slush
+    }
+    else if (precipChance < 60 && precipChance >= 30 && precipType === 'rain') {
+        // bring umbrella just in case
+    }
+    else {
+        // bruh you good
     }
 }
