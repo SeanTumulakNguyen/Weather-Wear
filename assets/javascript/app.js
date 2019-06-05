47879da7396c9c902ad96b61347ce42c561e6a60
Juwami <sean.nguyenf@yahoo.com>
2019-06-04 21:12:13 -0400

    pseudo code function for clothes to wear based on temp variable

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,19 @@
-// variable for HTML form for dates
-let dateInput = document.getElementById('#').value
 // variable for HTML form for gender preferences
 let genderInput = document.getElementById('#').value
 // variable for HTML form for zipcode location
 let locationInput = document.getElementById('#').value
 
+let temp
+let precipChance
+
+let clothesfromTemp = function() {
+    if (temp >= 70) {
+        // it's hot shorts and t-shirts
+    }
+    else if (temp < 70 && temp >= 50) {
+        // fall weather get a bit of clothes on
+    }
+    else (temp < 50) {
+        // tryin' to eat some frostbite boi?
+    }
+}
