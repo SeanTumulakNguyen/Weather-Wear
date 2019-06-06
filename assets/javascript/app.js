8200438ff76c3e37a90be2d37a31460b0efbd461
Juwami <sean.nguyenf@yahoo.com>
2019-06-06 18:37:48 -0400

    removed incoming changes from Develop branch

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,71 +1,45 @@
 // variable for HTML form for gender preferences
 let genderInput = document.getElementById('#').value
 // variable for HTML form for zipcode location
-// <<<<<<< Javascript
-// let locationInput = document.getElementById('#zipcode-input').value
+let locationInput = document.getElementById('#zipcode-input').value
 
-// let weatherView = document.getElementById('#weather-view')
-// let clothingView = documet.getElementById('#clothing-view')
+let weatherView = document.getElementById('#weather-view')
+let clothingView = documet.getElementById('#clothing-view')
 
-// document.onclick('#submit', function() {
+document.onclick('#submit', function() {
 
-// })
-// =======
-// let locationInput = document.getElementById('#').value
-// >>>>>>> Develop
+})
 
 let temp
 let precipChance
 let precipType = // this is from openweatherapi on type of precip. rain or snow?
 
-// <<<<<<< Javascript
-//     let clothesfromTemp = function () {
-//         if (temp >= 70) {
-//             // it's hot shorts and t-shirts
-//         } else if (temp < 70 && temp >= 50) {
-//             // fall weather get a bit of clothes on
-//         } else(temp < 50) {
-//             // tryin' to eat some frostbite boi?
-//         }
-//     }
-// =======
-// let clothesfromTemp = function() {
-//     if (temp >= 70) {
-//         // it's hot shorts and t-shirts
-//     }
-//     else if (temp < 70 && temp >= 50) {
-//         // fall weather get a bit of clothes on
-//     }
-//     else (temp < 50) {
-//         // tryin' to eat some frostbite boi?
-//     }
-// }
-// >>>>>>> Develop
+let clothesfromTemp = function() {
+    if (temp >= 70) {
+        // it's hot shorts and t-shirts
+    }
+    else if (temp < 70 && temp >= 50) {
+        // fall weather get a bit of clothes on
+    }
+    else (temp < 50) {
+        // tryin' to eat some frostbite boi?
+    }
+}
 
 let accessoriesrfromPrecip = function () {
     if (precipChance >= 60 && precipType === 'snow') {
-        // get your milk, eggs, and bread
-// <<<<<<< Javascript
-//     } else if (precipChance >= 60 && precipType === 'rain') {
-//         // keep umbrella on you
-//     } else if (precipChance < 60 && precipChance >= 30 && precipType === 'snow') {
-//         // shovels and slush
-//     } else if (precipChance < 60 && precipChance >= 30 && precipType === 'rain') {
-//         // bring umbrella just in case
-//     } else {
-// =======
-//     }
-//     else if (precipChance >= 60 && precipType === 'rain') {
-//         // keep umbrella on you
-//     }
-//     else if (precipChance < 60 && precipChance >= 30 && precipType === 'snow') {
-//         // shovels and slush
-//     }
-//     else if (precipChance < 60 && precipChance >= 30 && precipType === 'rain') {
-//         // bring umbrella just in case
-//     }
-//     else {
-// >>>>>>> Develop
-        // bruh you good
+        get your milk, eggs, and bread
+    }
+    else if (precipChance >= 60 && precipType === 'rain') {
+        // keep umbrella on you
+    }
+    else if (precipChance < 60 && precipChance >= 30 && precipType === 'snow') {
+        // shovels and slush
+    }
+    else if (precipChance < 60 && precipChance >= 30 && precipType === 'rain') {
+        // bring umbrella just in case
+    }
+    else {
+        bruh you good
     }
 }
