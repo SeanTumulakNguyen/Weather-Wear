b1236e35487ac087dcefe3e41b14ec76e12d7f5a
Ellen N Evans <ellennewellevans@MacPro-EEvans.local>
2019-06-06 19:54:57 -0400

    continued to work on API call for google

--- assets/javascript/googleajax.js
+++ assets/javascript/googleajax.js
@@ -14,13 +14,13 @@ var config = {
   // Create a variable to reference the database
   var database = firebase.database();
 
-
-  function displayMovieInfo() {
-
-    var queryURL = "https://cse.google.com/cse?cx=013791775854691782139:83btdvy04wk" + movie + "&apikey=AIzaSyAaYcg84hynl1DkmKZ7cjIo2u_-6D3udKg";
+    var queryURL = "https://www.googleapis.com/customsearch/v1?key=AIzaSyAaYcg84hynl1DkmKZ7cjIo2u_-6D3udKg";
 
     // Creating an AJAX call for the specific movie button being clicked
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
+        console.log(response)})
+        
+    
