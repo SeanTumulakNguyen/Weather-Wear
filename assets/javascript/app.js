b2b083c4b3cc3af05d6f230e9910403100c8c3fa
Juwami <sean.nguyenf@yahoo.com>
2019-06-06 19:48:19 -0400

    fixed pulling value of input for zipcode

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,33 +1,26 @@
-// variable for HTML form for gender preferences
-let genderInput = document.getElementsByName('gender').value
-console.log('Gender Chosen: ' + genderInput)
-// variable for HTML form for zipcode location
-let zipCodeInput = document.getElementById('#zipcode-input').value
-console.log('Zipcode Typed: ' + zipCodeInput)
-// radio button chosen for dates
-let dateTravel = document.getElementsByName('date').value
-console.log('Date Value Chosen: ' + dateTravel)
-let weatherView = document.getElementById('#weather-view')
-let clothingView = documet.getElementById('#clothing-view')
+let weatherView = document.getElementById('weather-view')
+let clothingView = document.getElementById('clothing-view')
 
+document.getElementById('submit').onclick = function() {
+    let genderInput = document.getElementsByName('gender').value
+    let zipCodeInput = document.getElementById('zipcode-input').value
+    let dateTravel = document.getElementsByName('date').value
 
-
-document.onclick('#submit', function() {
-
-})
+    console.log('Gender Chosen: ' + genderInput)
+    console.log('Zipcode Typed: ' + zipCodeInput)
+    console.log('Date Value Chosen: ' + dateTravel)
+}
 
 let temp
 let precipChance
-let precipType = // this is from openweatherapi on type of precip. rain or snow?
+let precipType // this is from openweatherapi on type of precip. rain or snow?
 
-let clothesfromTemp = function() {
+let clothesfromTemp = function () {
     if (temp >= 70) {
         console.log('Is is 70 degrees or more')
-    }
-    else if (temp < 70 && temp >= 50) {
+    } else if (temp < 70 && temp >= 50) {
         console.log('It is between 50 and 70 degrees')
-    }
-    else (temp < 50) {
+    } else if (temp < 50) {
         console.log('Is it below 50 degrees')
     }
 }
@@ -35,17 +28,13 @@ let clothesfromTemp = function() {
 let accessoriesrfromPrecip = function () {
     if (precipChance >= 60 && precipType === 'snow') {
         console.log('There is a great chance it will snow')
-    }
-    else if (precipChance >= 60 && precipType === 'rain') {
+    } else if (precipChance >= 60 && precipType === 'rain') {
         console.log('There is a great chance it will rain')
-    }
-    else if (precipChance < 60 && precipChance >= 30 && precipType === 'snow') {
+    } else if (precipChance < 60 && precipChance >= 30 && precipType === 'snow') {
         console.log('Chance of slush, flutters, and needing to shovel snow')
-    }
-    else if (precipChance < 60 && precipChance >= 30 && precipType === 'rain') {
+    } else if (precipChance < 60 && precipChance >= 30 && precipType === 'rain') {
         console.log('Bring an umbrella just in case')
-    }
-    else {
+    } else {
         console.log('We may be freezing')
     }
 }
