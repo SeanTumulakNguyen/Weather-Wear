29c9cb80205149779034c2b2f54073923a16534f
Ellen N Evans <ellennewellevans@MacPro-EEvans.local>
2019-06-08 13:33:00 -0400

    deleted seperate js folder and merged the code into app.js

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -132,3 +132,37 @@ weatherDisplay.appendChild(newRow);
 temperature = temp;
 weatherConditions = cond;
 }
+// logic to state in the search term the type of weather
+  
+let clothesType = function () {
+    if (temperature >= 75) {
+        clothesType = "warm+weather"
+    } else if (temperature < 75 && temperature >= 55) {
+      clothesType = "mild+weather"
+    } else if (temperature < 55) {
+      clothesType = "cold+weather"
+    }
+}
+
+let genderType = function () {
+  if (genderInputValue = 1) {
+      clothesType = "for+women"
+  } else {
+    clothesType = "for+men"
+  }
+}
+  //searchTerm will use the data sent back from the weather api and search for that locations fashion 
+  // we are using cold weather, mild weather, and hot weather ranges
+  var searchTerm = "clothesType+genderType"; //needs to take data from nick and covert to a search query
+  var queryURL = "https://www.googleapis.com/customsearch/v1?q=" + searchTerm + "&cx=013791775854691782139%3A83btdvy04wk&exactTerms=clothing&fileType=jpg&gl=United+States&imgSize=medium&imgType=photo&searchType=image&key=AIzaSyAaYcg84hynl1DkmKZ7cjIo2u_-6D3udKg";
+  
+
+    // Creating an AJAX call for the specific movie button being clicked
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+        console.log(response)})
+
+        // pull back 4 images for each days forecast
+        // make the images clickable and link to the link back in the JSON
